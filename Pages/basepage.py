b4786313dee2b3ab0deb83f8466e439fcbe319cf
RikinaManandhar <merymanandhar@gmail.com>
2025-06-17 11:22:23 +0545

    parametrize

--- Pages/basepage.py
+++ Pages/basepage.py
@@ -32,7 +32,8 @@ class BasePage:
         element = self.wait_for_visibility(locator)
         return element.text
         
-    def read_test_data(self, filename):
+    @staticmethod
+    def read_test_data(filename):
         filepath = os.path.join(os.path.dirname(__file__), '..', 'data', filename)
         with open(filepath, 'r') as file:
             return json.load(file)
